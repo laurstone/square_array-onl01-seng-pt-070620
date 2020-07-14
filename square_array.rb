70211df3c93fd299d4319379bc07ce40b6d02f36
Lauren Stone <lstone321@gmail.com>
2020-07-14 23:20:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
+
+
 def square_array(array)
   # your code here
+
+  array.each_with_object([1, 2, 3]) do |number, new_array|
+    new_array << number ** 2
+    puts new_array[number ** 2]
+  end
 end
